6ce0c79ff63e31c3e719ccf79ddbefa08085f6d5
Harshvardhan Singh <singhhar@oregonstate.edu>
2019-06-05 16:31:44 -0700

    Added assignment.js

--- models/assignment.js
+++ models/assignment.js
@@ -17,19 +17,46 @@ const AssingmentSchema = {
 //   isAdmin,
 
 async function getAssignmentById (id) {
-
+  const db = getDBReference();
+  const collection = db.collection('courses');
+  if (!ObjectId.isValid(id)) {
+    return null;
+  } else {
+    const results = await collection
+      .find({ _id: new ObjectId(id) })
+      .toArray();
+    return results[0];
+  }
 }
 
 exports.getAssignmentById = getAssignmentById;
 
 async function insertNewAssignment (assignment) {
-
+  course = extractValidFields(assignment, AssingmentSchema);
+  const db = getDBReference();
+  const collection = db.collection('assignments');
+  const result = await collection.insertOne(assignment);
+  return result.insertedId;
 }
+exports.insertNewCourse = insertNewCourse;
 
 exports.insertNewAssignment = insertNewAssignment;
 
-async function replaceAssignmentById (id) {
-
+async function replaceAssignmentById (id, body) {
+	const db = getDBReference();
+  const courseValues = {
+		"subject": body.subject,
+		"number": body.number,
+		"title": body.title,
+    "term": body.term,
+    "instructorId": body.instructorId
+    };
+    const collection = db.collection('assignments');
+    const result = await collection.replaceOne(
+      { _id: new ObjectId(id) },
+      courseValues
+    );
+    return result.matchedCount > 0;
 }
 
 exports.replaceAssignmentById = replaceAssignmentById;
