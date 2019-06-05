09f43d5715a380e2cf6fa607b731f09ab5f7b626
Nikhil Anand <nikhilanand249@gmail.com>
2019-06-04 19:41:30 -0700

    Working dockercompose and dbinit files

---
+++ db-init/01-db-init.js
@@ -0,0 +1,20 @@
+db.user.insertMany([
+  {
+      "name": "Rob Hess",
+      "email": "robhess@gmail.com",
+      "password": "hunter2",
+      "role":"instructor"
+    },
+    {
+        "name": "Nikhil Anand",
+        "email": "nikhil@gmail.com",
+        "password": "password",
+        "role":"student"
+    },
+    {
+        "name": "Harsh Singh",
+        "email": "admin@gmail.com",
+        "password": "password",
+        "role":"admin"
+    }
+])
